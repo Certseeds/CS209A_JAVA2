cf25f5b2e595a3434e704b9e354609b46d65035c
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-02-27 12:21:25 +0800

    initial.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ src/main/java/com/nanoseeds/App.java
@@ -0,0 +1,13 @@
+package com.nanoseeds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
