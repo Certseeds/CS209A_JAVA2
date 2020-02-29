353f5f1b8210305040fb03107bc8258e1d05b447
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-02-29 19:17:49 +0800

    add week01's code.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ src/main/java/com/nanoseeds/week01/BufferReader.java
@@ -0,0 +1,37 @@
+package com.nanoseeds.week01;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class BufferReader {
+	static final String file_path="src/main/java/com/nanoseeds/week01/sample.txt";
+
+	public static void main(String[] args) {
+		try (FileInputStream fis = new FileInputStream(new File(file_path));
+				InputStreamReader isr = new InputStreamReader(fis, "gb18030");
+				BufferedReader bReader = new BufferedReader(isr);){
+			
+
+			char[] cbuf = new char[65535];
+			int file_len = bReader.read(cbuf);
+
+			System.out.println(file_len);
+			System.out.println(cbuf);
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("The pathname does not exist.");
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("The Character Encoding is not supported.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Failed or interrupted when doing the I/O operations");
+			e.printStackTrace();
+		}
+	}
+}
