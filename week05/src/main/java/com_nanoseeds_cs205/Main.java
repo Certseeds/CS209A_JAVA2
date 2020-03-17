0a169b3fb5eb81c948cafb5863d89b561cbf8a63
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-03-17 23:01:38 +0800

    week05 now can run with maven, but the dependencies is still can not use.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- week05/src/main/java/com_nanoseeds_cs205/Main.java
+++ week05/src/main/java/com_nanoseeds_cs205/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com_nanoseeds_cs205;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         try {
             Parent root =
-                    FXMLLoader.load(getClass().getResource("mainUI.fxml"));
+                    FXMLLoader.load(getClass().getResource("/fxml/mainUI.fxml"));
             primaryStage.setTitle("My Application");
             primaryStage.setScene(new Scene(root));
             primaryStage.show();
